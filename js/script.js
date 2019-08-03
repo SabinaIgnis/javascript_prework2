3f2866bf1608b4775b228a3aeb59e5ac65209a49
Sabina Joecl <sabina.joeck@gmail.com>
2019-08-03 11:18:13 +0200

    Add final version

--- js/script.js
+++ js/script.js
@@ -29,3 +29,25 @@ if(randomNumber == 1){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+} else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Przegrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+} else if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Przegrywasz!');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Przegrywasz!');
+} else if (playerMove == 'nieznany ruch'){
+  printMessage('Nie słuchasz mnie!');
+}
