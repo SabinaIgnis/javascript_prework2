a8528e7d383797d0ad3397f3fa260ca0ba01896d
Sabina Joecl <sabina.joeck@gmail.com>
2019-08-03 11:08:45 +0200

    Add compuers moves

--- js/script.js
+++ js/script.js
@@ -13,3 +13,19 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+  computerMove = 'papier';
+} else if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
